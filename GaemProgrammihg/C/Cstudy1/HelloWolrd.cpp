a8b2ceff5642488f1b4166358fdda04452cc2b00
gydud0819 <pstar0819@naver.com>
2025-02-14 16:59:27 +0900

    (2025.02.14) first commit

---
+++ GaemProgrammihg/C/Cstudy1/HelloWolrd.cpp
@@ -0,0 +1,17 @@
+// <- 주석(컴퓨터는 알아볼 수 없다.)
+
+/* */ //(슬래시+shift+8)
+/*
+작성일 : 2025-02-14
+작성자 : 박효영
+주제		: hello world 출력하기
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello World");
+	return 0;
+}
